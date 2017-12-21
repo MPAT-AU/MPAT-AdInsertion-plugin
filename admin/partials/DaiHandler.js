70dfa59cddd1c83147f2d48640512ee093fb90d0
Fabian P <fabip123@gmail.com>
2017-12-21 15:33:25 +0100

    test

---
+++ admin/partials/DaiHandler.js
@@ -0,0 +1,9 @@
+// import React, { Component } from 'react';
+// import Request from 'react-http-request';
+ 
+function test(){
+  console.log("test");
+}
+
+ 
+
--- admin/partials/MPAT-AdInsertion-plugin-admin-display.php
+++ admin/partials/MPAT-AdInsertion-plugin-admin-display.php
@@ -14,3 +14,17 @@
 ?>
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
+<<!DOCTYPE html>
+<html>
+<head>
+    <!-- <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" /> -->
+    <script src="DaiHandler.js"></script>
+</head>
+<body>
+    <button onclick="test()">Click me</button
+</body>
+</html>
