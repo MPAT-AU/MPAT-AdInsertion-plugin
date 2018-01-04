3a0d7901265b7e00aab9eb00bd948b26fffba587
Fabian P <puch@campus.tu-berlin.de>
2018-01-04 13:49:22 +0100

    get request to server ready

--- admin/partials/DaiHandler.js
+++ admin/partials/DaiHandler.js
@@ -1,9 +1,34 @@
-// import React, { Component } from 'react';
-// import Request from 'react-http-request';
- 
-function test(){
-  console.log("test");
+
+function sendAndHandleRequest(jsonBody,name,thumbnail){
+
+  var dashUrl = ""
+  var hlsUrl  = ""
+
+  httpGetAsync("http://daiservices.fokus.fraunhofer.de:3001/json/fame/vod", jsonBody, response => {
+      console.log(response);
+
+      dashUrl = response['dashUrl'];
+      hlsUrl = response['hlsUrl'];
+
+      saveInDatabase(dashUrl, hlsUrl, name , thumbnail);
+
+  });
+
 }
 
+
+function httpGetAsync(theUrl, JSONBODY , callback)
+{
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() { 
+      if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+          callback(xmlHttp.responseText);
+  }
+  xmlHttp.open("GET", theUrl, true); 
+  xmlHttp.send(JSONBODY);
+}
  
 
+function saveInDatabase(dashUrl, hlsUrl, name , thumbnail){
+  
+}
